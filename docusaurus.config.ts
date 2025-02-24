67aa9b3697d2f05f3807584cafffe214b3a33df3
MeetMahadevwala <122471628+MeetMahadevwala@users.noreply.github.com>
2025-02-24 16:27:38 +0530

    Color Updated acc to landing page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,11 +7,12 @@ const config: Config = {
   tagline: 'All the docs you need!',
   favicon: 'img/favicon.ico',
 
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.github.io',
   baseUrl: '/',
 
   organizationName: 'peer-sense', // Replace with your GitHub org/user
   projectName: 'docs', // Replace with your repo name
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -28,7 +29,7 @@ const config: Config = {
         docs: {
           routeBasePath: '/', // Makes docs the default homepage
           sidebarPath: './sidebars.ts',
-          editUrl: 'https://github.com/your-org/your-repo/edit/main/docs/',
+          // editUrl: 'https://github.com/your-org/your-repo/edit/main/docs/',
         },
         blog: false, // Disable blog
         pages: false, // Disable extra pages
@@ -41,30 +42,37 @@ const config: Config = {
 
   themeConfig: {
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark', // Set default mode to dark
+      disableSwitch: true, // This should remove the toggle
+      respectPrefersColorScheme: false, // Ensure it doesn't change based on system settings
+    },
     navbar: {
       title: 'Docs',
+      style: 'dark',
       logo: {
         alt: 'Docs Logo',
         src: 'img/logo.svg',
       },
-      items: [
-        // {
-        //   type: 'docSidebar',
-        //   sidebarId: 'tutorialSidebar',
-        //   position: 'left',
-        //   label: 'Docs',
-        // },
-        {
-          href: 'https://github.com/your-org/your-repo',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
+
+      // items: [
+      //   // {
+      //   //   type: 'docSidebar',
+      //   //   sidebarId: 'tutorialSidebar',
+      //   //   position: 'left',
+      //   //   label: 'Docs',
+      //   // },
+      //   // {
+      //   //   href: 'https://github.com/your-org/your-repo',
+      //   //   label: 'GitHub',
+      //   //   position: 'right',
+      //   // },
+      // ],
     },
     footer: {
       style: 'dark',
       links: [],
-      copyright: `Copyright © ${new Date().getFullYear()} My Documentation. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} PeerSense Documentation.`,
     },
     prism: {
       theme: prismThemes.github,
